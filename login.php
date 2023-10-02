7f6109db45360e52031a8201a00f59e2e014c45d
AemKTP <kittipatthongkhun@gmail.com>
2023-10-02 22:40:29 +0700

    Update login.php

--- login.php
+++ login.php
@@ -16,6 +16,10 @@ $conn->close();
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="main.css">
     <title>Login Pizza</title>
@@ -26,7 +30,7 @@ $conn->close();
             <div class="col-12 col-md-6">
                 <div class="card">
                     <div class="card-body">
-                        <div class="card-image"><img src="https://png.pngtree.com/png-vector/20230318/ourmid/pngtree-modern-traditional-food-sasami-pizza-3d-model-png-image_6651528.png" alt="pizza_icon"></div>
+                        <div class="card-image"><img src="pizza.png" alt="pizza_icon"></div>
                         <h1 class="card-title"><b>Login</b></h1>
                         <form action="process_login.php" method="post">
                             <label for="email">Email</label><br>
