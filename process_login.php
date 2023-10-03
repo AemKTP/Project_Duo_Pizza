26b6316dff488039f785d72c4c613040421950c9
AemKTP <kittipatthongkhun@gmail.com>
2023-10-04 01:36:55 +0700

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -7,8 +7,20 @@ $password = $_POST['password'];
 $type = $_POST['type'];
 
 
-print_r($_POST);
+// print_r($_POST);
 
+// echo "Not Email"."<br>"; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
 $stmt = $conn->prepare("Select * from user");
 $stmt->execute();
 $result = $stmt->get_result();
@@ -16,15 +28,19 @@ $result = $stmt->get_result();
 while ($row = $result->fetch_assoc()){
 
     if($row['email'] == $email){
-        echo "success Email"."<br>"; 
-        echo $email;
         if (password_verify($password, $row['password'])) {
             if($type == $row['type']){
                 if($row['type'] == 'ลูกค้า'){
                     header("Location: customer.php");
+                    ?>
+                    
+
+                <?php
                 }
                 if($row['type'] == 'เจ้าของร้าน'){
                     header("Location: owner.php");
+
+                     
                 }
             }
         }
@@ -35,7 +51,6 @@ while ($row = $result->fetch_assoc()){
         header("Location: login.php");
     }
 }
-// echo "Not Email"."<br>"; 
-
-
-?>
+    ?>
+</body>
+</html>
