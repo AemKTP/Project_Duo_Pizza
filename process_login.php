59ae6a8ef2411c92838bfc51e9ea328d73513826
AemKTP <kittipatthongkhun@gmail.com>
2023-10-04 01:12:24 +0700

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -7,31 +7,6 @@ $password = $_POST['password'];
 $type = $_POST['type'];
 
 
-
-// // ดำเนินการตรวจสอบข้อมูลในฐานข้อมูล
-// $sql = "SELECT * FROM user WHERE email = '$email'";
-// $result = $conn->query($sql);
-
-// if ($result->num_rows > 0) {
-//     $row = $result->fetch_assoc();
-//     $hashedPassword = $row['password'];
-
-//     // ตรวจสอบรหัสผ่าน
-//     if (password_verify($password, $hashedPassword)) {
-//         // รหัสผ่านถูกต้อง
-//         header('Location: owner.php');
-//         exit();
-//     } else {
-//         // รหัสผ่านไม่ถูกต้อง
-//         header('Location: login.php?error=รหัสผ่านไม่ถูกต้อง');
-//         exit();
-//     }
-// } else {
-//     // ไม่พบบัญชีผู้ใช้
-//     header('Location: login.php?error=ไม่พบบัญชีผู้ใช้');
-//     exit();
-// }
-
 print_r($_POST);
 
 $stmt = $conn->prepare("Select * from user");
@@ -39,13 +14,6 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 while ($row = $result->fetch_assoc()){
-
-    // echo "<br>".$row['type'];
-    // echo $type;
-    // if (password_verify($password, $row['password'])) {
-    //     header("Location: owner.php");
-    // }
-    
 
     if($row['email'] == $email){
         echo "success Email"."<br>"; 
