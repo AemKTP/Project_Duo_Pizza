8f31a6a39cc82564ca6334bf568a781796d89c93
AemKTP <kittipatthongkhun@gmail.com>
2023-10-06 00:39:00 +0700

    Update show.php

--- show.php
+++ show.php
@@ -3,11 +3,6 @@
 include "dbconn.php";
 
 $pid = isset($_GET['pid']) ? $_GET['pid'] : null;
-$uid = isset($_GET['uid']) ? $_GET['id'] : null;
-$stmt = $conn->prepare("select * from user where uid = ?");
-$stmt->bind_param('i', $uid);
-$stmt->execute();
-$result = $stmt->get_result();
 ?>
 <!doctype html>
 <html>
@@ -19,7 +14,7 @@ $result = $stmt->get_result();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <title>pizza</title>
+    <title>pizzaShow</title>
 </head>
 <nav>
     <?php
