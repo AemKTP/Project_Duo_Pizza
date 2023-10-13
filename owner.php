40ab5f2198331f78dbb894191974ad3baa6fa8f7
AemKTP <kittipatthongkhun@gmail.com>
2023-10-14 01:03:31 +0700

    Update owner.php

--- owner.php
+++ owner.php
@@ -1,8 +1,6 @@
 <?php
 include 'dbconn.php';
 
-
-
 ?>
 
 <!DOCTYPE html>
@@ -23,42 +21,39 @@ include 'dbconn.php';
         ?>
     </nav>
     <style>
-        .center {
+        html,
+        body {
+            overflow-x: hidden;
+            background: red;
+            background: linear-gradient(50deg, rgba(255, 0, 0, 1)0%, rgba(255, 144, 0, 1)110%);
+            transition: background-color .5s ease;
+        }
+
+        .centercard {
+            display: flex;
+            height: 90vh;
+            justify-content: center;
+            align-items: center;
+
+        }
+
+        .checkbox {
             display: flex;
             align-items: center;
-            justify-content: center;
+        }
+
+        input[type="checkbox"] {
+            margin-left: -0.5em;
+        }
+
+        .table-wrapper {
+            overflow-y: scroll;
         }
 
         .card {
-            display: flex;
-            align-items: auto;
-            justify-content: center;
-            height: 300px;
+            height: 730px;
+            border-radius: 20px;
 
-        }
-
-        .card-image {
-            margin-top: 10px;
-            display: flex;
-            flex-direction: center;
-            align-items: center;
-            text-align: center;
-
-        }
-
-        .card-image img {
-            width: auto;
-            height: auto;
-        }
-
-        .card-title {
-            text-align: auto;
-        }
-
-        .bgcolor {
-            background: red;
-            background: linear-gradient(50deg, rgba(255, 0, 0, 1) 0%, rgba(255, 144, 0, 1) 110%);
-            transition: background-color 0.5s ease;
         }
     </style>
 </head>
@@ -66,53 +61,112 @@ include 'dbconn.php';
 <body class="bgcolor">
 
     <?php
-    $stmt = $conn->prepare("SELECT * FROM `order`, user where order.uid = user.uid ");
+    $stmt = $conn->prepare("SELECT `order`.oid as order_oid, `order`.odate  as order_odate, `order`.total_price as order_total_price
+                            , user.uid      as user_uid, user.name          as user_name, user.phone            as user_phone
+                            , user.email    as user_email, user.address     as user_address
+                            FROM `order`
+                            INNER JOIN user ON user.uid  = order.uid
+                            INNER JOIN cart ON cart.cartid  = order.cart_id
+                            ");
     $stmt->execute();
     $result = $stmt->get_result();
+
+
+
+
     ?>
-    <div class="container" style="margin-top: 5%;">
-        <div class="row">
-            <div class="col">
-                <div class="col-3">
 
-                    <select class="form-select form-select-lg mb-3 " name="type" style="margin-block: 2%;" onchange="this.form.submit()">
-                        <option value="">เลือกทั้งหมด</option>
-                    </select>
-                </div>
-
-                <?php while ($row = $result->fetch_assoc()) {
-                ?>
-                    <div class="card">
-                        <div class="row justify-content-between">
-                            <div class="row center" style="margin-left: 2%; margin-right: 2%;">
-                                <div class="col grid grid-cols-3 gap-5">
-                                    <div class="">
-                                        <h6>Order : <?= $row['oid'] ?></h6>
-                                    </div>
-                                </div>
-                                <div class="col grid grid-cols-3 gap-5" style="display: flex;">
-                                    <div>
-                                        <h6>User : <?= $row['uid'] ?></h6>
-                                        <h6>Name : <?= $row['name'] ?></h6>
-                                        <h6>Phone : <?= $row['phone'] ?></h6>
-                                        <h6>Email : <?= $row['email'] ?></h6>
-                                    </div>
-                                </div>
-                                <div class="col grid grid-cols-3 gap-5">
-                                    <div>
-                                        <h6>address : <?= $row['oid'] ?></h6>
-                                    </div>
-                                </div>
-                                <div class="col grid grid-cols-3 gap-5">
-                                    <div>
-                                        <h6>amount : <?= $row['oid'] ?></h6>
-                                    </div>
-                                </div>
-                            </div>
+    <div class="row centercard" style="margin-top: 3%;">
+        <div class="col">
+            <div class="container">
+                <div class="card" style="overflow-y: scroll;">
+                    <div class="row" style="margin-left: 2%; margin-right: 2%;">
+                        <div class="container" style="margin-block: 2%;">
+                            <h1 style="text-decoration: underline;">รายการสั่งซื้อของลูกค้า</h1>
                         </div>
-                    </div>
 
-                <?php } ?>
+                        <div class="row" style="margin-top: 0%;">
+                            <div class="col-2" style="display: flex; justify-content: center;">
+                                <h3>Order</h3>
+                            </div>
+                            <div class="col-2" style="display: flex; justify-content: center;">
+                                <h3>User</h3>
+                            </div>
+                            <div class="col-2" style="display: flex; justify-content: center;">
+                                <h3>Order_Time</h3>
+                            </div>
+                            <div class="col-2" style="display: flex; justify-content: center;">
+                                <h3>Pizza_List</h3>
+                            </div>
+                            <div class="col-2" style="display: flex; justify-content: center;">
+                                <h3>Price</h3>
+                            </div>
+                            <div class="col-2" style="display: flex; justify-content: center;">
+                                <h3>Status</h3>
+                            </div>
+
+
+                        </div>
+                        <form action="">
+
+                            <div class="row" style="display: flex; justify-content: center; align-items: center; ">
+                                <?php while ($row = $result->fetch_assoc()) {
+
+                                ?>
+                                    <div class="row" style="border: 2px solid black; margin-top: 1%;">
+                                        <div class="row" style="margin-left: 2%; margin-right: 2%; align-items: center;">
+                                            <div class="col-1">
+                                                <div>
+                                                    <h6>Order :</h6>
+                                                    <h6><?= $row['order_oid'] ?></h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-3" style="display: flex;">
+                                                <div>
+                                                    <h6>User : <?= $row['user_uid'] ?></h6>
+                                                    <h6>Name : <?= $row['user_name'] ?></h6>
+                                                    <h6>Phone : <?= $row['user_phone'] ?></h6>
+                                                    <h6>Email : <?= $row['user_email'] ?></h6>
+                                                    <h6>address : <?= $row['user_address'] ?></h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div>
+                                                    <h6>Odate : <?= $row['order_odate'] ?></h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div>
+                                                    <h6>Pizza_List : <?= $row['order_odate'] ?></h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div>
+                                                    <h6>Total_Price : <?= $row['order_total_price'] ?></h6>
+                                                </div>
+                                            </div>
+                                            <div class="col-2">
+                                                <div>
+                                                    <select style="width: 8rem; height: 2rem;" name="order_status" id="order_status">
+                                                        <option value="กำลังเตรียมออเดอร์">กำลังเตรียมออเดอร์</option>
+                                                        <option value="กำลังส่ง">กำลังส่ง</option>
+                                                        <option value="ส่งแล้ว">ส่งแล้ว</option>
+                                                        <option value="ยกเลิก">ยกเลิก</option>
+                                                    </select>
+
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                <?php } ?>
+                            </div>
+                        </form>
+
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
