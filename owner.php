e9bad262e5beeca904d6b980e567cd93ae03dde7
AemKTP <kittipatthongkhun@gmail.com>
2023-10-14 21:27:49 +0700

    Update owner.php

--- owner.php
+++ owner.php
@@ -1,6 +1,7 @@
 <?php
 include 'dbconn.php';
 
+$owneruid = $_GET['uid'];
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +14,6 @@ include 'dbconn.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="main.css">
-
 
     <nav>
         <?php
@@ -56,20 +56,28 @@ include 'dbconn.php';
 
         }
     </style>
+
 </head>
 
 <body class="bgcolor">
 
     <?php
-    $stmt = $conn->prepare("SELECT `order`.oid as order_oid, `order`.odate  as order_odate, `order`.total_price as order_total_price
-                            , user.uid      as user_uid, user.name          as user_name, user.phone            as user_phone
-                            , user.email    as user_email, user.address     as user_address
-                            FROM `order`
-                            INNER JOIN user ON user.uid  = order.uid
-                            INNER JOIN cart ON cart.oid  = order.oid
-                            ");
+    $stmt = $conn->prepare("SELECT * FROM `order`");
     $stmt->execute();
     $result = $stmt->get_result();
+    // $stmt = $conn->prepare("SELECT DISTINCT name, odate, oid, uid, total_price
+    //                         FROM `order`");
+    // $stmt->execute();
+    // $result = $stmt->get_result();
+    // $stmt = $conn->prepare("SELECT `order`.oid as order_oid, `order`.odate  as order_odate, `order`.total_price as order_total_price
+    //                         , user.uid      as user_uid, user.name          as user_name, user.phone            as user_phone
+    //                         , user.email    as user_email, user.address     as user_address
+    //                         FROM `order`
+    //                         INNER JOIN user ON user.uid  = order.uid
+    //                         INNER JOIN cart ON cart.oid  = order.oid
+    //                         ");
+    // $stmt->execute();
+    // $result = $stmt->get_result();
 
     ?>
 
@@ -93,10 +101,10 @@ include 'dbconn.php';
                                 <h3>Order_Time</h3>
                             </div>
                             <div class="col-2" style="display: flex; justify-content: center;">
-                                <h3>Pizza_List</h3>
+                                <h3>Price</h3>
                             </div>
                             <div class="col-2" style="display: flex; justify-content: center;">
-                                <h3>Price</h3>
+                                <h3>Pizza_List</h3>
                             </div>
                             <div class="col-2" style="display: flex; justify-content: center;">
                                 <h3>Status</h3>
@@ -104,43 +112,50 @@ include 'dbconn.php';
 
 
                         </div>
-                        <form action="">
+                        <div class="row" style="display: flex; justify-content: center; align-items: center; ">
+                            <?php
+                            while ($row = $result->fetch_assoc()) {
+                                if ($row['odate'] != 'null') {
 
-                            <div class="row" style="display: flex; justify-content: center; align-items: center; ">
-                                <?php while ($row = $result->fetch_assoc()) {
-
-                                ?>
-                                    <div class="row" style="border: 2px solid black; margin-top: 1%;">
+                            ?>
+                                    <div class="row" style="border: 2px solid black; margin-top: 1%; height: 200px;">
                                         <div class="row" style="margin-left: 2%; margin-right: 2%; align-items: center;">
-                                            <div class="col-1">
+                                            <div class="col-2">
                                                 <div>
                                                     <h6>Order :</h6>
-                                                    <h6><?= $row['order_oid'] ?></h6>
+                                                    <h6><?= $row['oid'] ?></h6>
                                                 </div>
                                             </div>
-                                            <div class="col-3" style="display: flex;">
+                                            <div class="col-2" style="display: flex;">
+                                                <form action="showdetail_owner.php?uid=<?= $owneruid ?>" method="post">
+                                                    <div>
+                                                        <input type="hidden" name="customeruid" id="customeruid" value="<?= $row['uid']?>">
+                                                        <input type="hidden" name="detail_uid" id="detail_uid" value="detail_<?= $row['uid'] ?>">
+                                                        <h6 style=" display:flex; justify-content: center;">User : <?= $row['uid'] ?></h6>
+                                                        <button type="submit" class="btn btn-primary">รายละเอียดลูกค้า</button>
+                                                    </div>
+
+                                                </form>
+                                            </div>
+
+                                            <div class="col-2">
                                                 <div>
-                                                    <h6>User : <?= $row['user_uid'] ?></h6>
-                                                    <h6>Name : <?= $row['user_name'] ?></h6>
-                                                    <h6>Phone : <?= $row['user_phone'] ?></h6>
-                                                    <h6>Email : <?= $row['user_email'] ?></h6>
-                                                    <h6>address : <?= $row['user_address'] ?></h6>
+                                                    <h6>Odate : <?= $row['odate'] ?></h6>
                                                 </div>
                                             </div>
                                             <div class="col-2">
                                                 <div>
-                                                    <h6>Odate : <?= $row['order_odate'] ?></h6>
+                                                    <h6>Total_Price : <?= $row['total_price'] ?></h6>
                                                 </div>
                                             </div>
                                             <div class="col-2">
-                                                <div>
-                                                    <h6>Pizza_List : <?= $row[''] ?></h6>
-                                                </div>
-                                            </div>
-                                            <div class="col-2">
-                                                <div>
-                                                    <h6>Total_Price : <?= $row['order_total_price'] ?></h6>
-                                                </div>
+                                                <form action="showdetail_owner.php?uid=<?= $owneruid ?>" method="post">
+
+                                                    <div>
+                                                        <input type="hidden" name="customeruid" id="customeruid" value="<?= $row['uid'] ?>">
+                                                        <button type="submit" class="btn btn-primary">รายละเอียดพิซซ่า</button>
+                                                    </div>
+                                                </form>
                                             </div>
                                             <div class="col-2">
                                                 <div>
@@ -157,9 +172,10 @@ include 'dbconn.php';
                                         </div>
                                     </div>
 
-                                <?php } ?>
-                            </div>
-                        </form>
+                            <?php }
+                            } ?>
+                        </div>
+
 
 
                     </div>
